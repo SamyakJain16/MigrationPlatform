882457cc4e91733b2e388dc379c0aeebd2baa683
samyakjain16 <149601132+samyakjain16@users.noreply.github.com>
2024-10-23 16:58:13 +1100

    Delete app directory

--- app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Create a blueprint
-main_bp = Blueprint('main', __name__)
-
-
-@main_bp.route("/")
-def home():
-    return "<h1>Welcome to Migration Portal API</h1>", 200
